26d91ae2c48fed1c4a7e16486bc1c7152b433623
AndradeNarciso <andradegregorio277@gmail.com>
2026-02-18 17:40:49 +0200

    loading backend

---
+++ inventary_management_system_backend/src/test/java/com/andrade/inventary_management_system_backend/InventaryManagementSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrade.inventary_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventaryManagementSystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
