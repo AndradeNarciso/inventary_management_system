25662cde4a4467e3bfab7e476b82785cb07e136d
AndradeNarciso <andradegregorio277@gmail.com>
2026-02-20 19:16:05 +0200

    UserDetail added

---
+++ inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/security/AuthUser.java
@@ -0,0 +1,55 @@
+package com.andrade.inventary_management_system_backend.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.andrade.inventary_management_system_backend.domain.User;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class AuthUser implements UserDetails {
+    private User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(user.getRole().name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
