b0fa9443cfb2072a58791afb963398aa5aa46c56
AndradeNarciso <andradegregorio277@gmail.com>
2026-02-22 20:27:44 +0200

    AuthFilter done

--- inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/security/AuthFilter.java
+++ inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/security/AuthFilter.java
@@ -2,7 +2,12 @@ package com.andrade.inventary_management_system_backend.security;
 
 import java.io.IOException;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.andrade.inventary_management_system_backend.exception.HeaderNotFoundException;
@@ -25,7 +30,27 @@ public class AuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String token = getTokenFromRequest(request);
+
+        try {
+            String token = getTokenFromRequest(request);
+            String email = jwtUtil.getUserNameFromToken(token);
+            UserDetails userDetails = customUserDetailService.loadUserByUsername(email);
+
+            if (StringUtils.hasText(email) && jwtUtil.isValidToken(token, userDetails)) {
+                log.info("Valid token found from user with email: {}", email);
+                UsernamePasswordAuthenticationToken autenticaionToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+
+                autenticaionToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(autenticaionToken);
+            }
+        } catch (Exception e) {
+            log.info("Can not Authenticate user ");
+        }
+
+        filterChain.doFilter(request, response);
 
     }
 
@@ -33,16 +58,14 @@ public class AuthFilter extends OncePerRequestFilter {
 
         String header = request.getHeader("Authorization");
 
-        if (header == null || header.isBlank()) {
+        if (!StringUtils.hasText(header)) {
             throw new HeaderNotFoundException("Authorization header not found");
         }
         if (!header.startsWith("Bearer ")) {
             throw new IllegalArgumentException("Invalid Authorization header format");
         }
+        return header.substring("Bearer ".length());
 
-        String extractedToken = header.substring("Bearer ".length());
-
-        throw new UnsupportedOperationException("Unimplemented method 'getTokenFromRequest'");
     }
 
 }
