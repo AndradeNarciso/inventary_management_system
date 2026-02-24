8f9936be474645384f73335773dec6decffca34d
AndradeNarciso <andradegregorio277@gmail.com>
2026-02-24 13:50:11 +0200

    load current user information done

--- inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/service/implementation/UserServiceImpl.java
+++ inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/service/implementation/UserServiceImpl.java
@@ -6,6 +6,8 @@ import java.util.UUID;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -55,7 +57,7 @@ public class UserServiceImpl implements UserService {
         } catch (Exception e) {
             throw new RequiredRoleException("Invalid Role");
         }
-
+        log.info("New user added");
         return Response.builder()
                 .status(HttpStatus.OK.value())
                 .message("User sucessfuly registered")
@@ -124,8 +126,13 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getCurrentLoggedInUser() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getCurrentLoggedInUser'");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new NotFoundException("Was not found loggend user"));
+
     }
 
 }
