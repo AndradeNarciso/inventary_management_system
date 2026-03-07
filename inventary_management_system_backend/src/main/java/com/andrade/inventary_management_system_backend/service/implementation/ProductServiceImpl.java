4dab0f5fe8d58bc69a6d29aca4cb3ad3f3fad238
AndradeNarciso <andradegregorio277@gmail.com>
2026-03-07 16:47:15 +0200

    sanitize image and save it

--- inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/service/implementation/ProductServiceImpl.java
+++ inventary_management_system_backend/src/main/java/com/andrade/inventary_management_system_backend/service/implementation/ProductServiceImpl.java
@@ -1,5 +1,9 @@
 package com.andrade.inventary_management_system_backend.service.implementation;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -17,10 +21,14 @@ import com.andrade.inventary_management_system_backend.repository.ProductReposit
 import com.andrade.inventary_management_system_backend.repository.SupplierRepository;
 import com.andrade.inventary_management_system_backend.service.ProductService;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
+@Slf4j
 
 public class ProductServiceImpl implements ProductService {
 
@@ -30,12 +38,19 @@ public class ProductServiceImpl implements ProductService {
     // private final SupplierRepository supplierRepository;
 
     private static final String DIRECTORY_IMAGE = System.getProperty("user.dir") + "/product-image/";
+    private final Integer imagaMaxSize = 10 * 1024 * 1024;
 
     @Override
     public Response createProduct(ProductDto productDto, MultipartFile image) {
 
         Category categoryProduct = categoryRepository.findById(productDto.getIdCategory())
                 .orElseThrow(() -> new NotFoundException("Undefined category"));
+
+        if (image == null || image.isEmpty()) {
+            throw new EmptyResourceException("Image undefined");
+        }
+
+        String imagePath = saveImage(image);
 
         Product product = Product.builder()
                 .price(productDto.getPrice())
@@ -44,15 +59,10 @@ public class ProductServiceImpl implements ProductService {
                 .description(productDto.getDescription())
                 .expireDate(productDto.getExpireDate())
                 .category(categoryProduct)
+                .imageUrl(imagePath)
                 .build();
 
-        if (image == null || image.isEmpty()) {
-            throw new EmptyResourceException("Image undefined");
-        }
         productRepository.save(product);
-
-        productRepository.save(product);
-
         return Response.builder()
                 .status(HttpStatus.CREATED.value())
                 .message("Product saved sucessfully")
@@ -87,6 +97,31 @@ public class ProductServiceImpl implements ProductService {
     public Response searchProduct(String value) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'searchProduct'");
+    }
+
+    private String saveImage(MultipartFile image) {
+
+        if (!image.getContentType().startsWith("image/") || image.getSize() > imagaMaxSize) {
+            throw new IllegalArgumentException("only image under 10MB is allowed");
+        }
+
+        File directory = new File(DIRECTORY_IMAGE);
+
+        if (!directory.exists()) {
+            directory.mkdir(); // making sure that dir exist
+            log.info("Directory to save image has been created");
+        }
+
+        String fileName = UUID.randomUUID() + "_" + image.getOriginalFilename();
+        String imagePath = DIRECTORY_IMAGE + fileName;
+
+        try {
+            File destinationFile = new File(imagePath);
+            image.transferTo(destinationFile);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Error saving image: " + e.getMessage());
+        }
+        return imagePath;
     }
 
 }
